1a216e37e8d1411c442d2e77b58e3b35a7586134
Amar Khamkar <71604396+Amark19@users.noreply.github.com>
2022-07-29 00:06:25 +0530

    first-commit

---
+++ linked-list.py
@@ -0,0 +1,22 @@
+class node:
+    def __init__(self,val=None):
+        self.val = val
+        self.next = None
+
+class linkedlist:
+    def __init__(self):
+        self.headval = None
+    def printlist(self):
+        printval = self.headval
+        while printval!=None:
+            print(printval.val)
+            printval = printval.next
+
+list1 = linkedlist()
+list1.headval = node(1)
+n2 = node(2)
+n3 = node(3)
+
+list1.headval.next = n2
+n2.next = n3
+list1.printlist()
