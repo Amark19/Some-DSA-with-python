49622f60e0ec67006752251d66babe8dde4e6375
Amar Khamkar <71604396+Amark19@users.noreply.github.com>
2022-10-31 10:55:15 +0530

    Create reverse-linked-list.py

---
+++ reverse-linked-list.py
@@ -0,0 +1,38 @@
+class node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+class linkedlist:
+    def __init__(self):
+        self.head = None
+    def reverse(self):
+        prev = None
+        nex = None
+        cur = self.head
+        # can be easily visualize by three pointers
+        #0th iteration
+        #none 0    1       2
+        #prev curr next
+        #1st iteration
+        #0    1       2
+        #prev curr next
+        while cur!=None:
+            nex = cur.next 
+            cur.next = prev 
+            prev = cur
+            cur = nex
+        self.head = prev
+    def printlist(self):
+        printval = self.head
+        while printval!=None:
+            print(printval.data)
+            printval = printval.next
+ls = linkedlist()
+ls.head = node(0)
+n1 = node(1)
+n2 = node(2)
+ls.head.next = n1
+n1.next = n2
+ls.printlist()
+ls.reverse()
+ls.printlist()
