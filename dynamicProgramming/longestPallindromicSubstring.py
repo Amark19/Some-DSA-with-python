a45d275b544fe2834aa295e711624db10534e1b0
Amar Khamkar <71604396+Amark19@users.noreply.github.com>
2022-11-13 03:18:54 +0530

    Update longestPallindromicSubstring.py

--- dynamicProgramming/longestPallindromicSubstring.py
+++ dynamicProgramming/longestPallindromicSubstring.py
@@ -1,13 +1,42 @@
 def longestPalindrome(self, s: str) -> str:
-    length,max_rep,idx,string = len(s),0,0,""
-    arr = [[0 for k in range(length+1)] for l in range(length+1)]
-    for i in range(length + 1):
-        for j in range(length + 1):
-            if (i == 0 or j == 0):arr[i][j] = 0
-            elif s[length - i] == s[j - 1] : arr[i][j] = arr[i-1][j-1] + 1
-            else:arr[i][j] = 0
-            if max_rep < arr[i][j]: idx = j;max_rep = arr[i][j]
-    for i in range(max_rep):
-        string += s[idx-i-1]
-    print(idx)
-    return string
+    #babad
+    #for a
+    #first check a then bab
+    #odd finish,now even
+    #ab then baba
+    #for b....
+    def palindrome(l,r,pall_len,pall):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            if (r-l + 1) > pall_len :
+                pall_len = r - l + 1
+                pall = s[l:r + 1]
+            l -= 1
+            r += 1
+        return pall,pall_len
+
+    pall = ""
+    pall_len = 0
+    for i in range(len(s)):
+        pall,pall_len = palindrome(i,i,pall_len,pall)#check for odd
+        pall,pall_len = palindrome(i,i+1,pall_len,pall)#then check for even
+    return pall
+    
+    
+    ##some optimizations
+    def palindrome(l,r,pall_len,pall):
+        res_l,res_r = l,r
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            if (r-l + 1) > pall_len :
+                pall_len = r - l + 1
+                res_l,res_r = l,r
+            l -= 1
+            r += 1
+        if (res_r-res_l + 1) >= pall_len and l != res_l and r != res_r:
+            pall = s[res_l:res_r + 1]
+        return pall,pall_len
+    pall,pall_len = "",0
+    for i in range(len(s)):
+        pall,pall_len = palindrome(i,i,pall_len,pall)#check for odd
+        pall,pall_len = palindrome(i,i+1,pall_len,pall)#then check for even
+    return pall
+                
