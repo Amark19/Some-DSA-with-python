f7a2a19abfb63b8114705760c2dc20db40117fbd
Amar Khamkar <71604396+Amark19@users.noreply.github.com>
2022-11-14 21:11:41 +0530

    Create merge-sorted-list.py

---
+++ linked-list/merge-sorted-list.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        curr1,curr2,curr3 = list1,list2,ListNode()
+        demo = curr3
+        while curr1 != None or curr2 != None :
+            if curr1 != None and curr2 == None:
+                curr3.next = ListNode(curr1.val)
+                curr3 = curr3.next
+            elif curr2 != None and curr1 == None:
+                curr3.next = ListNode(curr2.val)
+                curr3 = curr3.next
+            else:
+                if curr1.val >= curr2.val :
+                    curr3.next = ListNode(curr2.val)
+                    curr3.next.next = ListNode(curr1.val)
+                    curr3 = curr3.next.next
+                else:
+                    curr3.next = ListNode(curr1.val)
+                    curr3.next.next = ListNode(curr2.val)
+                    curr3 = curr3.next.next
+            
+            if curr1 != None: curr1 = curr1.next
+            if curr2 != None: curr2 = curr2.next
+        return demo.next
+            
