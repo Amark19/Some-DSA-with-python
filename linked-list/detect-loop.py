974a7d50e9a8525da283c9cd55b996bec366ef4e
Amar Khamkar <71604396+Amark19@users.noreply.github.com>
2022-11-20 00:22:38 +0530

    Create detect-loop.py

---
+++ linked-list/detect-loop.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        ls =[]
+        while head:
+            if head in ls:return 1
+            ls.append(head);head = head.next
+        return 0
