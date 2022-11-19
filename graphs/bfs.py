167fd34970cc4438c3939cb5acf61eb28cb5b91e
Amark19 <amarkhamkar6@gmail.com>
2022-11-19 16:22:14 +0530

    bfs done

--- graphs/bfs.py
+++ graphs/bfs.py
@@ -1,6 +1,5 @@
 from collections import defaultdict
 
-
 class graph:
     def __init__(self):
         self.graph = defaultdict(list)
@@ -8,18 +7,14 @@ class graph:
     def addEdge(self, src, dest):
         self.graph[src].append(dest)
 
-    def travserse(self, node, q, visited):
-        del q[0]
+    def travserse(self, node, queue, visited):
+        if node in visited:
+            return visited
+        del queue[0]
         for val in self.graph[node]:
-            # if val not in visited:
-            q.append(val)
-            # if val not in visited:
-            #     visited.add(val)
-            #     self.travserse(val, q, visited)
-            # else:
-            #     return visited 
+            queue.append(val)
         visited.add(node)
-        self.travserse(q[0],q,visited)
+        self.travserse(queue[0], queue, visited)
         return visited
 
 
@@ -29,9 +24,7 @@ g.addEdge(0, 2)
 g.addEdge(1, 4)
 g.addEdge(2, 3)
 g.addEdge(3, 4)
-q = []
-q.append(0)
+queue = []
+queue.append(0)
 visited = set()
-print(g.travserse(0, q, visited))
-# 0,1
-# 0,2
+print(g.travserse(0, queue, visited))
