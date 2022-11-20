3bb83b2d716ef4cca83402d5aa995781a04e4b20
Amark19 <amarkhamkar6@gmail.com>
2022-11-20 13:15:25 +0530

    some comments selection-sort

--- sorting/selection.py
+++ sorting/selection.py
@@ -2,9 +2,9 @@ def selectionSort(arr):
     for i in range(len(arr)):
         min = arr[i]
         for j in range(i,len(arr)):
-            if arr[j] < min:
+            if arr[j] < min:#we find minimum element at each pass and 
                 min = arr[j]
-                arr[i],arr[j] = arr[j],arr[i]
+                arr[i],arr[j] = arr[j],arr[i]#swap it to ith position
     return arr
 
 arr = [64, 25, 12, 22, 11]
