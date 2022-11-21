4e1ccf01bfa0181148dc29df82237ff27764cbda
Amark19 <amarkhamkar6@gmail.com>
2022-11-21 23:31:08 +0530

    insertion-sort added

---
+++ sorting/insertionSort.py
@@ -0,0 +1,12 @@
+def insertionSort(arr):
+    for i in range(len(arr) - 1):
+        for j in range(i+1,0,-1):
+            if arr[j] < arr[j-1]:
+                arr[j],arr[j-1] = arr[j-1],arr[j]
+
+
+
+if __name__ == "__main__":
+    arr = [12, 11, 13, 5, 6]
+    insertionSort(arr)
+    print(arr)
