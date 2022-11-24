a0e4e3ccd2665a8cc78f73a0c5ef3824ff476aea
Amar Khamkar <71604396+Amark19@users.noreply.github.com>
2022-11-24 23:25:00 +0530

    Update insertionSort.py

--- sorting/insertionSort.py
+++ sorting/insertionSort.py
@@ -1,6 +1,6 @@
 def insertionSort(arr):
     for i in range(len(arr) - 1):
-        for j in range(i+1,0,-1):
+        for j in range(i+1,0,-1):#sorting left side of array at each iteration
             if arr[j] < arr[j-1]:
                 arr[j],arr[j-1] = arr[j-1],arr[j]
 
