dbcf131dadc0583303955f0e7ab940a6dd9a5aec
Amar Khamkar <71604396+Amark19@users.noreply.github.com>
2022-12-09 00:56:40 +0530

    Update combination-sum.py

--- recursion/combination-sum.py
+++ recursion/combination-sum.py
@@ -1,1 +1,18 @@
-
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        N = len(candidates)
+        ans = []
+        def subsequence(ls,index,target,ans):
+            if index >= N or target < 0:
+                return
+            if target == 0:
+                ans.append(ls.copy())
+                print(ans)
+                return
+            ls.append(candidates[index])
+            subsequence(ls,index,target - candidates[index],ans)
+            ls.remove(candidates[index])
+            subsequence(ls,index + 1,target,ans)
+        subsequence([],0,target,ans)
+        print(ans)
+        return ans
