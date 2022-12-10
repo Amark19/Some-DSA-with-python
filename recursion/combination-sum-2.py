44ccd5eae4e3665e20a1172311968224d9c3f4a4
Amar Khamkar <71604396+Amark19@users.noreply.github.com>
2022-12-11 01:09:18 +0530

    Update combination-sum-2.py

--- recursion/combination-sum-2.py
+++ recursion/combination-sum-2.py
@@ -1,17 +1,15 @@
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        N = len(candidates)
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         ans = []
-        def subsequence(ls,index,target,ans):
-            if index >= N or target < 0:
+        nums.sort()
+        def subsequence(ls,idx):
+            if idx >= len(nums):
+                if ls not in ans:
+                    ans.append(ls.copy())
                 return
-            if target == 0:
-                ans.append(ls.copy())
-                return
-            ls.append(candidates[index])
-            subsequence(ls,index,target - candidates[index],ans)
-            ls.remove(candidates[index])
-            subsequence(ls,index + 1,target,ans)
-        subsequence([],0,target,ans)
-        print(ans)
+            ls.append(nums[idx])
+            subsequence(ls,idx + 1)
+            ls.remove(nums[idx])
+            subsequence(ls,idx + 1)
+        subsequence([],0)
         return ans
