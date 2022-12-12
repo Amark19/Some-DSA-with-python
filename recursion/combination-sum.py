401716afa8309df3f5be0f685ee57fdf0accf1c4
Amar Khamkar <71604396+Amark19@users.noreply.github.com>
2022-12-12 22:36:56 +0530

    Update combination-sum.py

--- recursion/combination-sum.py
+++ recursion/combination-sum.py
@@ -2,7 +2,7 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         N = len(candidates)
         ans = []
-        def subsequence(ls,index,target,ans):
+        def subsequence(ls,index,target):
             if index >= N or target < 0:
                 return
             if target == 0:
@@ -10,12 +10,10 @@ class Solution:
                 #but here as we r using same address for ls so whenever ls is modifying it's value then ans also modifies it's value
                 #so that's why we create copy which create new address/memory from which it will never modify again
                 ans.append(ls.copy())
-                print(ans)
                 return
             ls.append(candidates[index])
-            subsequence(ls,index,target - candidates[index],ans)
+            subsequence(ls,index,target - candidates[index])
             ls.remove(candidates[index])
-            subsequence(ls,index + 1,target,ans)
-        subsequence([],0,target,ans)
-        print(ans)
+            subsequence(ls,index + 1,target)
+        subsequence([],0,target)
         return ans
