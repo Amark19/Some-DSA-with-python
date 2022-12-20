67f98e7e2183ab6a90f5c39454ec277e874634be
Amark19 <amarkhamkar6@gmail.com>
2022-12-20 23:04:31 +0530

    minor changes

--- arrays/sort-array-set-bits.py
+++ arrays/sort-array-set-bits.py
@@ -1,5 +1,8 @@
-
-# Using custom comparator lambda function
-arr = [31,2,3,5,13]
-
-print(sorted(arr, key = lambda x: (bin(x).count('1'), x)))
+from operator import itemgetter
+arr = [0,1,2,3,4,5,6,7,8]
+ls,ans = [],[]
+for val in arr:
+    ls.append((val,bin(val)[2:].count('1')))
+for val in sorted(ls,key=itemgetter(1,0)):
+    ans.append(val[0])
+print(ans)
