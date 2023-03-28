163929c7a12672d155775e611f43dad4726f9304
Amar Khamkar <71604396+Amark19@users.noreply.github.com>
2023-03-28 23:59:38 +0530

    Create minimum-path-sum.py

---
+++ dynamicProgramming/minimum-path-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        row,col = len(grid),len(grid[0])
+        res = [[float("inf")] * (col + 1) for i in range(row+1)]
+        res[row-1][col] = 0
+
+        for r in range(row - 1,-1,-1):
+            for c in range(col - 1,-1,-1):
+                res[r][c] = grid[r][c] + min(res[r+1][c],res[r][c+1])
+        return res[0][0]
