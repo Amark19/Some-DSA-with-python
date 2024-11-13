39c3b9d8a0423d59c1c3f9ffce0d63eaf8af5e5e
Amark19 <amarkhamkar6@gmail.com>
2024-11-14 00:50:06 +0530

    Added description for Kosaraju's algorithm

--- graphs/strongly-connected-graph.py
+++ graphs/strongly-connected-graph.py
@@ -52,3 +52,32 @@ class Solution:
                 self.dfs(vis, adjR, [], tp_ele)
 
         return scc  # Return the number of strongly connected components
+
+
+"""
+strongly connected components are the components in a graph where every node is reachable from every other node in the component.
+if u->v is reachable then v->u should also be reachable
+
+IN KOSARAJU'S ALGORITHM:
+Consider a graph,
+0 <- 2 -> 3 -> 4 -> 7
+| /            | \  | 
+1              5 -> 6
+
+0, 1, 2 are strongly connected components
+3 is a strongly connected component
+4, 5, 6 are strongly connected components
+7 is a strongly connected component
+
+The algorithm works as follows:
+1. Perform a DFS traversal on the original graph and keep track of the finishing order of nodes.
+2. Reverse the edges of the graph to create the transposed graph.
+3. Process nodes in the reverse order of finishing time to find strongly connected components.(dfs)
+
+Why This Order Matters for SCCs
+In Kosaraju’s algorithm, the order of nodes based on their finishing times has a specific purpose:
+
+Source Nodes in SCCs: In a directed graph, SCCs are like "closed loops" where every node in the SCC can reach every other node in that SCC. However, the SCC itself can be thought of as having a "source" node when looking at connections between different SCCs.
+
+Processing in Reverse Order: When we reverse the graph, starting from nodes in the reverse finishing order ensures that we handle each SCC separately without accidentally merging nodes from different SCCs. In essence, it helps isolate each SCC.
+"""
